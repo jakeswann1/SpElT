2b2bf5ea7dd73e01582c5dd977c490ec71a91f23
Jake Swann <jakeswann3@hotmail.co.uk>
2023-11-28 09:06:23 +0000

    Code to calculate CSD by theta cycle

--- analysis/bz_csd.py
+++ analysis/bz_csd.py
@@ -1,14 +1,17 @@
+import pandas as pd
+import numpy as np
+
 def bz_csd(lfp, **kwargs):
     """
     Calculates the 1D approximation of current source density (CSD) from a linear array of LFPs.
     Translated from: https://github.com/buzsakilab/buzcode/blob/master/analysis/lfp/CurrentSourceDensity/bz_CSD.m
 
     Args:
-        lfp (ndarray or dict): LFP data. If ndarray, shape should be (timepoints, channels). If dict, should have fields 'data', 'timestamps', 'samplingRate'.
+        lfp (ndarray or dict): LFP data. If ndarray, shape should be (timepoints, channels). If dict, should have fields 'data', 'timestamps', 'sampling_rate'. If pandas DataFrame, should have timestamps as columns, channel IDs as rows
         **kwargs: Optional keyword arguments for customizing the CSD computation and plotting.
 
     Returns:
-        dict: CSD data with fields 'data', 'timestamps', 'samplingRate', 'channels', 'params'.
+        dict: CSD data with fields 'data', 'timestamps', 'sampling_rate', 'channels', 'params'.
     """
     
     import numpy as np
@@ -19,34 +22,51 @@ def bz_csd(lfp, **kwargs):
     if isinstance(lfp, dict):
         data = lfp['data']
         timestamps = lfp['timestamps']
-        samplingRate = lfp['samplingRate']
+        sampling_rate = lfp['sampling_rate']
+
+    elif isinstance(lfp, pd.DataFrame):
+        data = lfp.to_numpy().T
+        timestamps = lfp.columns.to_numpy()
+        sampling_rate = kwargs.get('sampling_rate', 1000)
+        
     else:
         data = lfp
         timestamps = np.arange(data.shape[0])
-        samplingRate = kwargs.get('samplingRate', 1250)
+        sampling_rate = kwargs.get('sampling_rate', 1000)
 
     channels = kwargs.get('channels', np.arange(data.shape[1]))
     spat_sm = kwargs.get('spat_sm', 11)
     temp_sm = kwargs.get('temp_sm', 7)
-    doDetrend = kwargs.get('doDetrend', False)
-    plotCSD = kwargs.get('plotCSD', True)
-    plotLFP = kwargs.get('plotLFP', True)
+    do_detrend = kwargs.get('do_detrend', False)
+    plot_csd = kwargs.get('plot_csd', True)
+    plot_lfp = kwargs.get('plot_lfp', True)
     win = kwargs.get('win', [0, data.shape[0]])
 
     # Compute CSD
     lfp_frag = data[win[0]:win[1], channels] * -1
+    
+    if lfp_frag.size == 0:
+        print("LFP fragment is empty. Skipping processing.")
+        return None
 
     # Detrend
-    if doDetrend:
+    if do_detrend:
         lfp_frag = detrend(lfp_frag, axis=0)
-
+        
     # Temporal smoothing
-    if temp_sm > 0:
-        lfp_frag = savgol_filter(lfp_frag, window_length=temp_sm, polyorder=3, axis=0)
+    if temp_sm > 0 and lfp_frag.shape[0] > temp_sm:
+        # Ensure window length is odd and appropriate
+        temp_sm = min(temp_sm | 1, lfp_frag.shape[0] - 1)
+        polyorder_temp = min(3, temp_sm - 1)
+        lfp_frag = savgol_filter(lfp_frag, window_length=temp_sm, polyorder=polyorder_temp, axis=0)
 
     # Spatial smoothing
-    if spat_sm > 0:
-        lfp_frag = savgol_filter(lfp_frag, window_length=spat_sm, polyorder=3, axis=1)
+    if spat_sm > 0 and lfp_frag.shape[1] > spat_sm:
+        # Ensure window length is odd and appropriate
+        spat_sm = min(spat_sm | 1, lfp_frag.shape[1] - 1)
+        polyorder_spat = min(3, spat_sm - 1)
+        lfp_frag = savgol_filter(lfp_frag, window_length=spat_sm, polyorder=polyorder_spat, axis=1)
+
 
     # Calculate CSD
     CSD = np.diff(lfp_frag, n=2, axis=0)
@@ -55,12 +75,12 @@ def bz_csd(lfp, **kwargs):
     csd = {}
     csd['data'] = CSD
     csd['timestamps'] = timestamps[win[0] + 2: win[1]]  # Remove the adjustment by 2
-    csd['samplingRate'] = samplingRate
+    csd['sampling_rate'] = sampling_rate
     csd['channels'] = channels
-    csd['params'] = {'spat_sm': spat_sm, 'temp_sm': temp_sm, 'detrend': doDetrend}
+    csd['params'] = {'spat_sm': spat_sm, 'temp_sm': temp_sm, 'detrend': do_detrend}
 
     # Plot
-    if plotLFP:
+    if plot_lfp:
         cmax = np.max(np.abs(data[:, channels]))
         plt.figure()
         plt.imshow(data[win[0]:win[1], channels].T, aspect='auto', cmap='seismic', vmin=-cmax, vmax=cmax)
@@ -70,7 +90,7 @@ def bz_csd(lfp, **kwargs):
         plt.title('LFP')
         plt.show()
 
-    if plotCSD:
+    if plot_csd:
         cmax = np.max(np.abs(CSD))
         fig, axes = plt.subplots(1, 2, figsize=(10, 5))
         axes[0].contourf(csd['timestamps'], np.arange(CSD.shape[1]), CSD.T, 40, cmap='jet', vmin=-cmax, vmax=cmax)
@@ -86,3 +106,37 @@ def bz_csd(lfp, **kwargs):
         plt.show()
 
     return csd
+
+def calculate_csd_df(arm_cycle_df):
+    '''
+    Takes dataframe of LFP data with theta cycles and traversal indices
+    Calculates current-source density for each traversal individually and adds to the dataframe
+    '''
+    # Add indices to output dataframe
+    csd_index = [label + '_csd' for label in arm_cycle_df.drop(['Traversal Index', 'Cycle Index', 'Cycle Theta Phase']).index]
+    csd_df_empty = pd.DataFrame(np.nan, index = csd_index, columns = arm_cycle_df.columns)
+    csd_df = arm_cycle_df.append(csd_df_empty, ignore_index = False)
+    
+    # Get total number of traversals
+    traversals = int(max(arm_cycle_df.loc(axis = 0)['Traversal Index']))
+    
+    # Loop through traversals and calculate CSD
+    for traversal in range(traversals):
+        
+        # Select traversal data from dataframe
+        traversal_df = arm_cycle_df.loc[:, arm_cycle_df.loc['Traversal Index'] == traversal]
+        
+        # Calculate CSD
+        traversal_csd = bz_csd(traversal_df.drop(['Traversal Index', 'Cycle Index', 'Cycle Theta Phase'], axis = 0),
+                              plot_csd = False,
+                              plot_lfp = False)
+        
+        # If CSD calculation failed
+        if traversal_csd == None:
+            print(f'Skipping traversal {traversal}, not enough data')
+        else:
+            # CSD has two fewer samples than LFP, so align CSD to correct timestamps and add to frame
+            csd_df.loc[csd_index, traversal_df.columns[1:-1]] = traversal_csd['data'].T
+
+        
+    return csd_df
