ec645fafebe3136d448ac4fe5adb9eaca2cb9ce2
Jake Swann <jakeswann3@hotmail.co.uk>
2024-02-08 17:45:42 +0000

    Save place cell inclusion to dataframe

--- analysis/spatial_significance.py
+++ analysis/spatial_significance.py
@@ -2,8 +2,6 @@ import numpy as np
 import pynapple as nap
 from spatial_information import spatial_info
 from postprocessing.rate_maps import make_rate_maps
-from postprocessing.adaptive_smooth import adaptive_smooth
-
 from joblib import Parallel, delayed
 
 def compute_shuffle(spike_times_real, pos_sample_times, pos_bin_idx, pos_sampling_rate):
