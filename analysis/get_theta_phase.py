3026244e3f1b94543a8cbe7bdfe5784f6a939bda
Jake Swann <jakeswann3@hotmail.co.uk>
2024-02-09 16:43:22 +0000

    Minor fixes

--- analysis/get_theta_phase.py
+++ analysis/get_theta_phase.py
@@ -28,7 +28,7 @@ def get_theta_phase(lfp, sampling_rate, peak_freq, clip_value = 0, filt_half_ban
     analytic_signal = hilbert(filtered_lfp)
     theta_phase = np.angle(analytic_signal)
     # Wrap into the range 0 - 2pi, so that oscillation starts at the peak (convention).
-    theta_phase = np.mod(theta_phase, 2 * np.pi)  
+    theta_phase %= 2 * np.pi 
 
     # Identify and handle phase transitions and slips
     phase_transitions = np.diff(theta_phase) < -np.pi
@@ -39,17 +39,20 @@ def get_theta_phase(lfp, sampling_rate, peak_freq, clip_value = 0, filt_half_ban
     # Remove any cycles with clipped values above or below 32000
     clipped_cycles = np.where(np.abs(lfp) > clip_value)[0]
 
+    # Calculate bincount
+    bincount_cycle_numbers = np.bincount(cycle_numbers)
+
     # Calculate power per cycle and identify bad power cycles
     power = filtered_lfp ** 2
-    power_per_cycle = np.bincount(cycle_numbers, power) / np.bincount(cycle_numbers)
+    power_per_cycle = np.bincount(cycle_numbers, power) / bincount_cycle_numbers
     power_threshold = np.nanpercentile(power_per_cycle, power_thresh)
-    bad_power_cycles = np.where(power_per_cycle < power_threshold)[0]
+    bad_power_cycles = power_per_cycle < power_threshold
 
     # Calculate cycle lengths and identify bad length cycles
-    cycle_lengths = np.bincount(cycle_numbers)
+    cycle_lengths = bincount_cycle_numbers
     pass_band = np.array([peak_freq - filt_half_bandwidth, peak_freq + filt_half_bandwidth])
     cycle_length_lim = np.ceil(1 / pass_band * sampling_rate).astype(int)
-    bad_length_cycles = np.where((cycle_lengths < cycle_length_lim[1]) | (cycle_lengths > cycle_length_lim[0]))[0]
+    bad_length_cycles = (cycle_lengths < cycle_length_lim[1]) | (cycle_lengths > cycle_length_lim[0])
 
     # Combine bad power and length cycles
     bad_cycles = np.union1d(bad_power_cycles, bad_length_cycles)
@@ -63,7 +66,7 @@ def get_theta_phase(lfp, sampling_rate, peak_freq, clip_value = 0, filt_half_ban
 
     return theta_phase, cycle_numbers
 
-def get_spike_theta_phase(lfp, spike_times, sampling_rate, peak_freq, filt_half_bandwidth=2, power_thresh=5):
+def get_spike_theta_phase(lfp, spike_times, sampling_rate, peak_freq, clip_value = 0, filt_half_bandwidth=2, power_thresh=5):
     """
     Calculate the theta phase of spikes based on the LFP.
 
@@ -79,7 +82,7 @@ def get_spike_theta_phase(lfp, spike_times, sampling_rate, peak_freq, filt_half_
     - spike_phases: Theta phase (in radians) of spikes.
     """
 
-    theta_phase, _ = get_theta_phase(lfp, sampling_rate, peak_freq, filt_half_bandwidth, power_thresh)
+    theta_phase, _ = get_theta_phase(lfp, sampling_rate, peak_freq, clip_value = clip_value, filt_half_bandwidth = filt_half_bandwidth, power_thresh = power_thresh)
     
     # MAP SPIKE TIMES TO PHASE
     spike_indices = (spike_times * sampling_rate).astype(int)
