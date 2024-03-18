e92d4ba716c73320c64cbd401e8b94a1cf3d8b7d
Jake Swann <jakeswann3@hotmail.co.uk>
2024-03-18 16:43:15 +0000

    Neuropixels 2 OpenEphys Kilosort 4 sorting added

---
+++ pyscan/preprocessing/np2_preprocessing.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import spikeinterface as si
+import spikeinterface.sorters as ss
+
+def sort_np2(recording, recording_name, base_folder, sorting_suffix):
+
+    sorting_path = Path(f'{base_folder}/{recording_name[:6]}_{sorting_suffix}')
+
+    if (sorting_path).is_dir():
+        try:
+            sorting = si.load_extractor(sorting_path / 'sorter_output')
+            print(f"Sorting loaded from file {sorting_path}\n")
+        except ValueError:
+            print(f"Sorting at {sorting_path} failed to load - try deleting the folder and rerun")
+            raise ValueError
+    else:
+        # Sort
+        sorting = ss.run_sorter('kilosort4', recording, output_folder=f'{sorting_path}',
+                                        verbose = True, docker_image = False, remove_existing_folder = True)
+
+        print(f'Recording sorted!\n KS4 found {len(sorting.get_unit_ids())} units\n')
+        sorting = sorting.remove_empty_units()      
+        sorting.save(folder=sorting_path / 'sort')
+        print(f'Sorting saved to {sorting_path}/sort\n')
+        
